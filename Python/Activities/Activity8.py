2e403afbd5674dd0ed028ce5e71e32adfadfb6c7
sireesha-pentela <Sireesha.Pentela@ibm.com>
2025-09-18 02:25:51 +0530

    Add files via upload

---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,9 @@
+num_list = [90, 21, 75, 28, 10, 90]
+first_value = num_list[0]
+last_value = num_list[-1]
+print("First and last values are equaal?")
+if first_value == last_value:
+    print("True")
+else:
+    print("False")
+        
