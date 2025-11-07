0a0a8396356e3d8d1eba69f4fe4bc032f3718c6e
sireesha-pentela <Sireesha.Pentela@ibm.com>
2025-11-07 12:53:14 +0530

    Update TestActivity1.java

--- TestNG/Activities/TestActivity1.java
+++ TestNG/Activities/TestActivity1.java
@@ -1,19 +1,48 @@
+
 package Activities;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class TestActivity1 {
-  @Test
-  public void f() {
-  }
-  @BeforeClass
-  public void beforeClass() {
-  }
-
-  @AfterClass
-  public void afterClass() {
-  }
-
+    WebDriver driver;
+    
+    // Setup function
+    @BeforeClass
+    public void setUp() {
+        // Initialize Firefox driver
+        driver = new FirefoxDriver();
+        // Open the page
+        driver.get("https://training-support.net");
+    }
+    
+    // Test function
+    // The 'priority' attribute in the @Test annotation determines the order in which test methods are executed.
+    // Lower values indicate higher priority. This test will run before others with higher priority values.
+    @Test(priority = 1)
+    public void homePageTest() {
+        // Assert page title
+        Assert.assertEquals(driver.getTitle(), "Training Support");
+        
+        // Find and click the About page link
+        driver.findElement(By.linkText("About Us")).click();
+    }
+    
+    @Test(priority = 2)
+    public void aboutPageTest() {
+        // Assert page title
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+    }
+    
+    // Teardown function
+    @AfterClass
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
 }
